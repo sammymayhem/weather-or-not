6f597568874687e4f16834977bc6c0851f04b861
Sam W <sam@sammymayhem.com>
2023-02-03 18:12:02 -0800

    Added the fetch component to the main App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import CitySearch from './components/CitySearch';
 import './App.css';
 import { Container } from 'react-bootstrap';
+import UseFetch from './hooks/UseFetch';
+import { API_KEY, API_BASE_URL } from './apis/config';
 
 const App = () => {
+
+  const {data, error, isLoading, setUrl} = UseFetch();
+
   return (
     <Container className="App">
-      <CitySearch />
+      <CitySearch
+        onSearch={(city) => setUrl(`${API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}`)} />
     </Container>
   );
 };
