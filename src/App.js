f17abb71b94f89c9e7a2c2365d536f742e0e0f57
Sam W <sam@sammymayhem.com>
2023-02-03 16:48:14 -0800

    Finished creating Weather card with temp info to display at this time.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import Weather from './components/Weather';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Weather
+        dt={1602104400 * 1000}
+        temp_min='22.67'
+        temp_max='24.39'
+        main='Clear'
+        icon='01d'
+      />
     </div>
   );
 }
